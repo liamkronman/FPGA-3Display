d7ef98b926442045d8eb37cafb6367d922a4553d
JT <markowitzjt@gmail.com>
2024-11-19 17:49:33 -0500

    Scripts folder for creating a full build cycle, also creating .mem files

---
+++ scripts/fill_tables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def arctan_2_lookup():
+    pass
+
+def sqrt_lookup():
+    pass
+
+def main():
+    arctan_2_lookup()
+    sqrt_lookup()
+
+if __name__ == '__main__':
+    main()
