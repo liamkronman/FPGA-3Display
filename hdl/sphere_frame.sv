4e9a349dd00945a1049d3b301ae1cc5d2a98ded0
Liam Kronman <lkronman10@stuy.edu>
2024-11-26 02:00:39 -0500

    simpler sphere frame

--- hdl/sphere_frame.sv
+++ hdl/sphere_frame.sv
@@ -14,34 +14,43 @@ module sphere_frame #(
     
     // PLAN: approach this mathematically first, and explore BRAM/SRAM if that method starts choking.
 
-    localparam int RADIUS = NUM_ROWS / 2; // NUM_ROWS == NUM_ROWS for our system
-    localparam int CENTER_X = NUM_COLS / 2;
-    localparam int CENTER_Y = NUM_ROWS / 2;
-
-    logic [$clog2(NUM_ROWS)-1:0][RGB_RES-1:0] column1, column2;
-    logic [$clog2(SCAN_RATE)-1:0] x1, x2, y_offset;
-
     always_comb begin
+        // Set all rows in both columns to high
         for (int y = 0; y < NUM_ROWS; y++) begin
-            column1[y] = '0;
-            column2[y] = '0;
+            columns[0][y] = {RGB_RES{1'b1}}; // Set all bits in RGB_RES to 1
+            columns[1][y] = {RGB_RES{1'b1}}; // Set all bits in RGB_RES to 1
         end
-
-        for (int y = 0; y < NUM_ROWS; y++) begin
-            x1 = column_index1 - CENTER_X;
-            x2 = column_index2 - CENTER_X;
-            y_offset = y - CENTER_Y;
-
-            if ((x1 * x1 + y_offset * y_offset) <= (RADIUS * RADIUS)) begin
-                column1[y] = {RGB_RES{1'b1}};;
-            end
-            if ((x2 * x2 + y_offset * y_offset) <= (RADIUS * RADIUS)) begin
-                column2[y] = {RGB_RES{1'b1}};;
-            end
-        end
-
-        columns[0] = column1;
-        columns[1] = column2;
     end
+
+    // localparam int RADIUS = NUM_ROWS / 2; // NUM_ROWS == NUM_ROWS for our system
+    // localparam int CENTER_X = NUM_COLS / 2;
+    // localparam int CENTER_Y = NUM_ROWS / 2;
+
+    // logic [$clog2(NUM_ROWS)-1:0][RGB_RES-1:0] column1, column2;
+    // logic [$clog2(SCAN_RATE)-1:0] x1, x2, y_offset;
+
+    // always_comb begin
+    //     for (int y = 0; y < NUM_ROWS; y++) begin
+    //         column1[y] = '0;
+    //         column2[y] = '0;
+    //     end
+
+    //     for (int y = 0; y < NUM_ROWS; y++) begin
+    //         x1 = column_index1 - CENTER_X;
+    //         x2 = column_index2 - CENTER_X;
+    //         y_offset = y - CENTER_Y;
+
+    //         if ((x1 * x1 + y_offset * y_offset) <= (RADIUS * RADIUS)) begin
+    //             column1[y] = {RGB_RES{1'b1}};;
+    //         end
+    //         if ((x2 * x2 + y_offset * y_offset) <= (RADIUS * RADIUS)) begin
+    //             column2[y] = {RGB_RES{1'b1}};;
+    //         end
+    //     end
+
+    //     columns[0] = column1;
+    //     columns[1] = column2;
+    // end
+
 endmodule
 `default_nettype none
