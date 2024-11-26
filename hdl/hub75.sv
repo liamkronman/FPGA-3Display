6b49a424af1e4ae1c192d43ff4648a05f14100ce
Nathaniel Felleke <nfelleke@mit.edu>
2024-11-25 19:00:56 -0500

    hub75 test

---
+++ hdl/hub75.sv
@@ -0,0 +1,91 @@
+
+`default_nettype none
+module hub75_output #(
+    parameter ROTATIONAL_RES=180,
+    parameter NUM_COLS=64,
+    parameter NUM_ROWS=64,
+    parameter SCAN_RATE=32,
+    parameter THETA_RES=8
+)
+ (
+    input wire rst_in,
+    input wire clk_in,
+    //input wire [1:0][NUM_ROWS-1:0][8:0] columns,
+    input wire [$clog2(SCAN_RATE)-1:0] col_index,
+    
+    output logic [2:0] rgb0,
+    output logic [2:0] rgb1,
+
+    output logic led_latch, 
+    output logic led_clk,
+    output logic led_output_enable
+
+    /*output logic         chunk_tvalid,
+    input wire           chunk_tready,
+    output logic [127:0] chunk_tdata,
+    output logic         chunk_tlast*/
+);
+
+   logic [5:0] pixel_counter;
+   logic [5:0] period_counter;
+   logic [2:0] state;
+   logic [2:0] pwm_counter; 
+
+   logic clk_msk; 
+   assign led_clk = clk_in & clk_msk; //control the hub75 clk input with the clk_msk  
+
+   always_ff @(posedge clk_in) begin
+    if(rst_in) begin
+        state <= 0;
+        pixel_counter <= 0 ;
+        clk_msk <= 0;
+    end
+    else if(state == 0) begin //initing
+        led_output_enable <= 1;
+        state <= 1;
+        pwm_counter <= 0;
+        pixel_counter <= 0;
+        led_latch <= 0;
+        clk_msk <= 1;
+
+
+        
+    
+    end
+    else if(state == 1) begin //BCM
+
+        led_output_enable <= 1;
+        /*rgb0[0] <= rows[0][pixel_counter][0 + pwm_counter];
+        rgb0[1] <= rows[0][pixel_counter][3 + pwm_counter];
+        rgb0[2] <= rows[0][pixel_counter][6 + pwm_counter];
+
+        rgb1[0] <= rows[1][pixel_counter][0 + pwm_counter];
+        rgb1[1] <= rows[1][pixel_counter][3 + pwm_counter];
+        rgb1[2] <= rows[1][pixel_counter][6 + pwm_counter];*/
+        rgb0[0] <= 1;
+        rgb0[1] <= 1; 
+        rgb0[2] <= 1;
+        rgb1[0] <= 1;
+        rgb1[1] <= 1; 
+        rgb1[2] <=1;
+        
+        if(pixel_counter == 63 ) begin
+            state <= 2;
+            clk_msk <= 0;
+        end
+        else begin
+            pixel_counter <= pixel_counter + 1;
+        end
+    end
+    else if(state == 2) begin //WAITING PERIOID
+    led_latch <=1;
+    state <= 0;
+
+    
+
+
+
+    end
+
+   end
+endmodule
--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -1,9 +1,16 @@
+
 `default_nettype none
 module top_level
 (
     input wire sysclk, // 12MHz clock (from CMOD A7)
     input wire ir_tripped, // PMOD pin 1
-    output logic [1:0] led // LED outputs
+    output logic [1:0] led, // LED outputs,
+    output logic [4:0] hub75_addr,
+    output logic [2:0] hub75_rgb0,
+    output logic [2:0] hub75_rgb1,
+    output logic hub75_latch,
+    output logic hub75_OE, 
+    output logic hub75_clk
 );
     // tie led0 to ir_led_control and led1 to low
     ir_led_control ilc(
@@ -11,5 +18,24 @@ module top_level
         .led_out(led[0])
     );
     assign led[1] = 0;
+
+    //TODO: CREATE 100 Mhz clock
+
+    //TODO: Create 20 MHZ clock
+    assign hub75_addr = 16;
+
+    hub75_output hub75 (
+        .rst_in(0),
+        .clk_in(sysclk),
+        .col_index(hub75_addr),
+        .rgb0(hub75_rgb0),
+        .rgb1(hub75_rgb1),
+        .led_latch(hub75_latch),
+        .led_output_enable(hub75_OE),
+        .led_clk(hub75_clk)
+    );
+
+
 endmodule
+
 `default_nettype none
