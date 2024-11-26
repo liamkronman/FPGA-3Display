283142b48312429972fee961b0369c623417b887
Liam Kronman <lkronman10@stuy.edu>
2024-11-25 22:36:36 -0500

    frame templates

---
+++ hdl/cube_frame.sv
@@ -0,0 +1,16 @@
+`default_nettype none
+module cube_frame #(
+    parameter SCAN_RATE=32,
+    parameter NUM_ROWS=64,
+    parameter RGB_RES=9,
+    parameter THETA_RES=27
+)
+(
+    input wire [THETA_RES-1:0] theta,
+    input wire [$clog2(SCAN_RATE)-1:0] column_index1,
+    input wire [$clog2(SCAN_RATE)-1:0] column_index2,
+    output logic [1:0][$clog2(NUM_ROWS)-1:0][RGB_RES-1:0] columns // presented column1, column2 in array
+);
+
+endmodule
+`default_nettype none
---
+++ hdl/sphere_frame.sv
@@ -0,0 +1,14 @@
+`default_nettype none
+module sphere_frame #(
+    parameter SCAN_RATE=32,
+    parameter NUM_ROWS=64,
+    parameter RGB_RES=9
+)
+(
+    input wire [$clog2(SCAN_RATE)-1:0] column_index1,
+    input wire [$clog2(SCAN_RATE)-1:0] column_index2,
+    output logic [1:0][$clog2(NUM_ROWS)-1:0][RGB_RES-1:0] columns // presented column1, column2 in array
+);
+
+endmodule
+`default_nettype none
