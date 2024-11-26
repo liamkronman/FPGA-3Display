0e1842d2af8eb804ed95712b3e06fe7aa36cc097
Liam Kronman <lkronman10@stuy.edu>
2024-11-26 02:01:44 -0500

    changed col_index2 width

--- hdl/sphere_frame.sv
+++ hdl/sphere_frame.sv
@@ -7,7 +7,7 @@ module sphere_frame #(
 )
 (
     input wire [$clog2(SCAN_RATE)-1:0] column_index1,
-    input wire [$clog2(SCAN_RATE)-1:0] column_index2,
+    input wire [$clog2(SCAN_RATE):0] column_index2,
     output logic [1:0][$clog2(NUM_ROWS)-1:0][RGB_RES-1:0] columns // presented column1, column2 in array
 );
     // given the two column indices, return the points on the circle that lie on those columns
